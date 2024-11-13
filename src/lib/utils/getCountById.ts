5b155fb3c6ddadaef70dc69a61202d2693708cef
Nelli Diachkina <nelli.diachkina@gmail.com>
2024-11-13 15:18:00 +0200

    fix api

--- src/lib/utils/getCountById.ts
+++ src/lib/utils/getCountById.ts
@@ -4,6 +4,7 @@ const getCountById = <T extends string, I extends Record<T, string>>(
 ) =>
   items.reduce(
     (counts, { [idKey]: id }) => {
+      if (!id) return counts;
       if (!counts[id]) counts[id] = 0;
       counts[id] += 1;
       return counts;
